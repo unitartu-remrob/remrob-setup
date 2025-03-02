b7fc0b137b65820e9f1f8360454333fd33fa7a49
Tsapu <chooky823@gmail.com>
2025-03-02 20:43:13 +0200

    remrob-docker role

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-ansible-playbook ansible/install.yaml --ask-become-pass --extra-vars="app_name=remrob-app"
-
-bash ./image-build.sh --clone-dir $HOME/remrob-app
+ansible-playbook ansible/install.yaml --ask-become-pass --extra-vars="app_name=remrob-app" --tags=common
